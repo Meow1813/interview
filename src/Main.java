810392dd1079c0a8720a739324794fc3df74d0b9
Alex <shurik228154@mail.ru>
2023-10-05 21:48:51 +0300

    Билет №15

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        for(int i = 0; i < arr.length/2;i++){
-            int tmp = arr[i];
-            arr[i]=arr[arr.length-i-1];
-            arr[arr.length-i-1]=tmp;
+        System.out.println(method(arr));
+    }
+    public static List<Integer> method(int[] arr){
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                list.add(arr[i]+1);
+            }else{
+                list.add(arr[i]);
+            }
         }
+        return list.subList(2,7);
     }
 }
